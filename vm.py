988945ad138850a91621962aa0f9e542eb4df2ca
Jon Cantwell <jon.c.cantwell@gmail.com>
2014-04-26 04:23:47 -0400

    set up opcode dispatch

--- vm.py
+++ vm.py
@@ -1,5 +1,6 @@
-class Memory:
+import sys, struct
 
+class Memory:
 	def __init__(self):
 		self.store = []
 
@@ -13,9 +14,14 @@ class Memory:
 		return self.store[memloc]
 
 class Vm:
+	codes = ['halt', 'set', 'push', 'pop', 'eq', 'gt', 'jmp', 'jt', 'jf', 'add', 'mult', 'mod', 'and', 'or', 'not', 'rmem', 'wmem', 'call', 'ret', 'out', 'in', 'noop']
 
 	def __init__(self):
 		self.memory = Memory()
+		self.stack = []
+		self.position = 0
+		self.running = True
+		self.unpacker = struct.Struct('<H')
 
 	def loadFile(self, filename):
 		dump = []
@@ -32,13 +38,103 @@ class Vm:
 				else:
 					break
 
-	def debug(self):
-		print ("Hey look, it's working!")
-		size_of_memory = self.memory.size()
-		print("Size of memory is {0}".format(size_of_memory))
-		print('Memory at memloc 100 is {0}'.format(self.memory.at(100)))
+	def run(self):
+		dispatch = {
+			'halt': self.opcodeHalt,
+			'set': self.opcodeSet,
+			'push': self.opcodePush,
+			'pop': self.opcodePop,
+			'eq': self.opcodeEq,
+			'gt': self.opcodeGt,
+			'jmp': self.opcodeJmp,
+			'jt': self.opcodeJt,
+			'jf': self.opcodeJf,
+			'add': self.opcodeAdd,
+			'mult': self.opcodeMult,
+			'mod': self.opcodeMod,
+			'and': self.opcodeAnd,
+			'or': self.opcodeOr,
+			'not': self.opcodeNot,
+			'rmem': self.opcodeRmem,
+			'wmem': self.opcodeWmem,
+			'call': self.opcodeCall,
+			'ret': self.opcodeRet,
+			'out': self.opcodeOut,
+			'in': self.opcodeIn,
+			'noop': self.opcodeNoop
+		}
+
+		while self.running:
+			instruction = self.memory.at(self.position)
+			print("Instruction read as {0}, converting to integer to lookup opcode...".format(instruction))
+			code = Vm.codes[struct.unpack('<H', instruction)[0]]
+			print("Encountered opcode {0}, calling function...".format(code))
+			dispatch[code]()
+
+	def advance(self, increment=1):
+		self.position += increment
+
+	def opcodeHalt(self):
+		"""stop execution and terminate the program. syntax: 0"""
+		print("HALT")
+		self.running = False
+
+	def opcodeSet(self):
+		self.advance()
+	def opcodePush(self):
+		self.advance()
+	def opcodePop(self):
+		self.advance()
+	def opcodeEq(self):
+		self.advance()
+	def opcodeGt(self):
+		self.advance()
+	def opcodeJmp(self):
+		self.advance()
+	def opcodeJt(self):
+		self.advance()
+	def opcodeJf(self):
+		self.advance()
+	def opcodeAdd(self):
+		self.advance()
+	def opcodeMult(self):
+		self.advance()
+	def opcodeMod(self):
+		self.advance()
+	def opcodeAnd(self):
+		self.advance()
+	def opcodeOr(self):
+		self.advance()
+	def opcodeNot(self):
+		self.advance()
+	def opcodeRmem(self):
+		self.advance()
+	def opcodeWmem(self):
+		self.advance()
+	def opcodeCall(self):
+		self.advance()
+	def opcodeRet(self):
+		self.advance()
+	
+	def opcodeOut(self):
+		"""write the character represented by ascii code <a> to the terminal. syntax: 19 a"""
+		print("OUT")
+		self.advance()
+		a = self.resolve(self.memory.at(self.position))
+		sys.stdout.write(a)
+		self.advance()
+	
+	def opcodeIn(self):
+		self.advance()
+	
+	def opcodeNoop(self):
+		self.advance()
+
+
+
 
 vm = Vm()
+print("VM initialised, loading challenge.bin...")
 vm.loadFile("challenge.bin")
-
-vm.debug()
+print("Challenge binary loaded into memory. Running VM...")
+vm.run()
